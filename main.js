c4ec1f37b869115be9a9522e1f9e1604b338e2de
Jerrad Moon <jerradm85@gmail.com>
2020-04-22 17:53:49 -0700

    updated zomato call

--- main.js
+++ main.js
@@ -17,10 +17,12 @@
 //GEOCODE: http://open.mapquestapi.com/geocoding/v1/address?key=KEY&location=${CITY},${STATE}
 
 
+//---Code Start---//
+
+
+//global variables and constants
 const mapQuestKey = 'nz5VddD23ce9UIWdEbxB4pe2tM6YEyOD';
-const zamatoApiKey = '6d305f760284a82816236872c2cd5935';
-const fourSqClientId = 'G1NU4QN1WCJA5RJS5UVORD3TQGZQOUI1Y3DGWGRXNA5KI5CM';
-const fourSqClientSecret = 'SN2D4HMIXTEGGP15EEMKRBI3GO4ORGFONTF4XEOJOVPHGZC1';
+const zomatoApiKey = '6d305f760284a82816236872c2cd5935';
 let origin = []; //lat[0], lng[1]
 let destination = []; //lat[0], lng[1]
 
@@ -44,60 +46,9 @@ function content() {
 }
 //-----End APP UI-----//
 
-// function getQueryParams(params) {
-// 	const queryItems = Object.keys(params)
-// 		.map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-// 	return queryItems.join('&');
-// }
 
 
-function getFourSqData(destinationCity, destinationState) {
-	const url = `https://api.foursquare.com/v2/venues/search?client_id=G1NU4QN1WCJA5RJS5UVORD3TQGZQOUI1Y3DGWGRXNA5KI5CM&client_secret=SN2D4HMIXTEGGP15EEMKRBI3GO4ORGFONTF4XEOJOVPHGZC1&v=20200421&near=${destinationCity},${destinationState}&categoryId=4d4b7104d754a06370d81259&limit=1`;
-
-	fetch(url)
-	.then(response => response.json())
-	.then(responseJson => console.log(responseJson))
-}
-
-function getZomatoData(destinationCity, destinationState) {
-	const url = 'https://developers.zomato.com/api/v2.1/collections?city_id=279&lat=47.6205&lon=122.3493&count=1';
-	
-	const options = {
-		headers: new Headers({
-			"user-key": zamatoApiKey
-		})
-	};
-	console.log(destinationCity, destinationState);
-	console.log(options);
-
-	fetch(url, options)
-	.then(response => response.json())
-	.then(responseJson => console.log(responseJson))
-
-}
-
-function watchForm(){
-	$("form").submit(e => {
-	
-		e.preventDefault();
-		
-		let startCity = $("#startCity").val();
-		let startState = $("#js-search1").find(":selected").val();
-		
-		let destinationCity = $("#destinationCity").val();
-		let destinationState = $("#js-search2").find(":selected").val();
-		
-		getOrigin(startCity, startState);	
-		getDestination(destinationCity, destinationState);
-		getFourSqData(destinationCity, destinationState);
-		getZomatoData(destinationCity, destinationState);	
-		
-	});
-}
-
-$(watchForm);
-
-//FETCH REQUESTS
+//---Fetch Requests---//
 function getOrigin(city, state){ //function fetches start city, state pair and passes object to a lat and lng function
 	
 	const url = `http://open.mapquestapi.com/geocoding/v1/address?key=${mapQuestKey}&location=${city},${state}`;
@@ -116,8 +67,8 @@ function getDestination(city, state){ //function fetches destintion city and sta
 	const url = `http://open.mapquestapi.com/geocoding/v1/address?key=${mapQuestKey}&location=${city},${state}`;
 	
 	console.log(url);
-	
-	fetch(url)
+
+	return fetch(url)
 		.then(response => response.json())
 		.then(responseJson => destinationLatandLng(responseJson))
 		.catch(err => alert("Wrong address"));
@@ -127,6 +78,7 @@ function originLatandLng(responseJson){
 	
 	console.log("origin lat: ", responseJson.results[0].locations[0].latLng.lat);
 	
+	origin = [];
 	origin.push(responseJson.results[0].locations[0].latLng.lat);
 	origin.push(responseJson.results[0].locations[0].latLng.lng);
 	
@@ -137,13 +89,80 @@ function originLatandLng(responseJson){
 function destinationLatandLng(responseJson){
 	
 	console.log("destination lat: ", responseJson.results[0].locations[0].latLng.lat);
+	console.log("destination lng: ", responseJson.results[0].locations[0].latLng.lng);
 
+	destination = [];
 	destination.push(responseJson.results[0].locations[0].latLng.lat);
 	destination.push(responseJson.results[0].locations[0].latLng.lng);
 		
 	console.log(destination);
+}
+
+function getZomatoLocationData(city, destination) {
+	const url = `https://developers.zomato.com/api/v2.1/locations?query=${city}&lat=${destination[0]}&lon=${destination[1]}&count=1`
+	const options = {
+		headers: new Headers({
+			"user-key": zomatoApiKey
+		})
+	};
+
+	fetch(url, options)
+		.then(response => response.json())
+		.then(responseJson => {
+			getZomatoCollectionData(responseJson.location_suggestions[0].entity_id, destination)
+		})
+}
+
+function getZomatoCollectionData(cityId, destination) {
+	const url = `https://developers.zomato.com/api/v2.1/collections?city_id=${cityId}&lat=${destination[0]}&${destination[1]}&count=1`;
+
+	const options = {
+		headers: new Headers({
+			"user-key": zomatoApiKey
+		})
+	};
+
+	fetch(url, options)
+	.then(response => response.json())
+	.then(responseJson => console.log(responseJson))
 
 }
+
+function getFourSqData(city, state) {
+	const fourSqClientId = 'G1NU4QN1WCJA5RJS5UVORD3TQGZQOUI1Y3DGWGRXNA5KI5CM';
+	const fourSqClientSecret = 'SN2D4HMIXTEGGP15EEMKRBI3GO4ORGFONTF4XEOJOVPHGZC1';
+
+	const url = `https://api.foursquare.com/v2/venues/search?client_id=${fourSqClientId}&client_secret=${fourSqClientSecret}&v=20200421&near=${city},${state}&categoryId=4d4b7104d754a06370d81259&limit=1`;
+
+	fetch(url)
+	.then(response => response.json())
+	.then(responseJson => console.log(responseJson))
+}
+//---End Fetch Requests---//
+
+function watchForm(){
+	$("form").submit(e => {
+	
+		e.preventDefault();
+		
+		let startCity = $("#startCity").val();
+		let startState = $("#js-search1").find(":selected").val();
+		
+		let destinationCity = $("#destinationCity").val();
+		let destinationState = $("#js-search2").find(":selected").val();
+		
+		getOrigin(startCity, startState);	
+		getDestination(destinationCity, destinationState)
+		.then(data => {
+			getFourSqData(destinationCity, destinationState);
+			getZomatoLocationData(destinationCity, destination);	
+		})
+		
+	});
+}
+
+$(watchForm);
+
 //function getSameDayWeather(lat, lon){
 	//const url = ``;
 	
